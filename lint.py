be6e8f36df2c5114647e0e0f3aa7f58633f9043f
Alexander Rodrigues <alex.rodrigues@mail.utoronto.ca>
2013-02-09 00:12:57 -0500

    Update lint.py

--- lint.py
+++ lint.py
@@ -2,8 +2,6 @@ import subprocess
 import json
 import sys
 
-#print content
-
 p = subprocess.Popen(["pyflakes", sys.argv[1]], stdout=subprocess.PIPE)
 
 out, err = p.communicate()
@@ -13,13 +11,10 @@ data = {}
 
 out = out.split("\n")[:-1]
 
-#print sys.argv[1]
-
 for error in out:
 	
 	error = error.split(":")
 	line = error[1]
-	#print line
 	errormsg = error[2]
 	data[line] = errormsg.strip()
 
